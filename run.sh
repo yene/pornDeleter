b0bb52edbc4f676022a496adbf99d623ab2b685b
Yannick Weiss <contact@yannickweiss.com>
2016-10-03 20:13:07 +0200

    Also cleaning up Safari and Chrome.
    But chrome and firefox still keep the url in the search box history.

--- run.sh
+++ run.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-# Defining firefox database 
-DBPATH="/home/USER/.mozilla/firefox/USERID/places.sqlite";
+# Close all browsers before running this script.
 
-#for each line in hosts file, check firefox history database for matching url and delete it
-cat hosts | while read line
-do
-   sqlite3 "$DBPATH" "DELETE FROM moz_places WHERE url LIKE '%$line%'";
+# Firefox
+find ~/Library/Application\ Support/Firefox/Profiles -name "places.sqlite" | while read path; do
+  echo "Processing Firefox file '$path'"
+  cat hosts | while read host
+  do
+    sqlite3 "$path" "DELETE FROM moz_places WHERE url LIKE '%$host%'";
+    # TODO: remove from URL bar
+    # TODO: remove cookies
+  done
 done
 
+# Safari
+echo "Processing Safari"
+cat hosts | while read host
+do
+  sqlite3 ~/Library/Safari/History.db "DELETE FROM history_items WHERE url LIKE '%$host%'";
+  # TODO: cleanup the history_visits by id
+  # TODO: remove cookies
+done
 
+# Chrome
+echo "Processing Chrome"
+cat hosts | while read host
+do
+  sqlite3 ~/Library/Application\ Support/Google/Chrome/Default/History "DELETE FROM urls WHERE url LIKE '%$host%'";
+  sqlite3 ~/Library/Application\ Support/Google/Chrome/Default/History "DELETE FROM segments WHERE name LIKE '%$host%'";
+  sqlite3 ~/Library/Application\ Support/Google/Chrome/Default/Shortcuts "DELETE FROM omni_box_shortcuts WHERE url LIKE '%$host%'";
+  sqlite3 ~/Library/Application\ Support/Google/Chrome/Default/Cookies "DELETE FROM cookies WHERE host_key LIKE '%$host%'";
+  # TODO: remove from URL bar
+  # TODO: cleanup the visits by id
+done
